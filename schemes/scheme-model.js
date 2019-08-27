264037bc5060b226d343b0ac1845058526f68ee9
anthonyvigz <anthonyvigz@gmail.com>
2019-08-27 19:54:18 -0400

    created and tested get steps for certain schemes knex function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,8 @@ const db = require('../data/db-config');
 
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps
 }
 
 function find() {
@@ -11,4 +12,12 @@ function find() {
 
 function findById(id) {
     return db('schemes').where({ id }).first();
+}
+
+function findSteps(scheme_id) {
+    return db('schemes')
+        .join('steps', 'schemes.id', 'steps.scheme_id')
+        .select('schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+        .where({ scheme_id })
+        .orderBy('step_number')
 }
